b0b5262675e5b0d3370d736d32d36656c1abb5dd
e-m-dev <eric.murray.dev@gmail.com>
2025-10-05 21:15:47 +0100

    chore: init spring boot project setup

---
+++ src/main/java/com/emdev/emailrelay/EmailrelayApplication.java
@@ -0,0 +1,13 @@
+package com.emdev.emailrelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailrelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailrelayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emdev/emailrelay/EmailrelayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emdev.emailrelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailrelayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
